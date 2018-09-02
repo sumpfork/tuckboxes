00ebed9a37db2cb8b07ebf9f64ba1a31a356ef6a
Peter <sandflea@mailmight.com>
2018-09-02 14:10:37 -0700

    make basic package

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+version = '0.0.1'
+
+setup(
+    name="tuckboxes",
+    version=version,
+    entry_points={
+        'console_scripts': [
+            "tuckboxes = tuckboxes.tuckboxes:main"
+        ],
+    },
+    packages=find_packages(exclude=['tests']),
+    install_requires=["reportlab>=3.4.0",
+                      "Pillow>=4.1.0",
+                      "numpy"],
+    url='http://domtabs.sandflea.org',
+    include_package_data=True,
+    author="Peter Gorniak",
+    author_email="sumpfork@mailmight.net",
+    description="Tuckbox Generation for Board- and Cardgames",
+    keywords=['boardgame', 'cardgame', 'tuckboxes'],
+    long_description=""
+)
